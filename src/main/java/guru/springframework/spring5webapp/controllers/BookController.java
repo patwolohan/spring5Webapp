2cbdd0dea919a91bf3e59257e3c57e4983c7c9b9
patwolohan <pwolohan@gmail.com>
2025-08-29 08:24:05 +0100

    Book and Author list displayed in browser.

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author: Pat Wolohan
+ * User:pwolo
+ * Date:28/08/2025
+ * Time:16:14
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+        return "books/list";
+    }
+}
